c4e37573bce654982aa125b92528e214e32209e1
ackerm17 <ackerm17@yahoo.com>
2024-07-27 12:47:16 -0400

    Alchemist update

--- scenes/light/point_light_2d.gd
+++ scenes/light/point_light_2d.gd
@@ -18,8 +18,7 @@ var is_damagable = true
 func _process(_delta: float) -> void:
 	player_in_light()
 	#TODO change, add a pathing for enemys instead of looking
-	if player:
-		look_at(player.global_position)
+
 
 func player_in_light():
 	# this was a pain
--- scenes/wizard/wizard.gd
+++ scenes/wizard/wizard.gd
@@ -3,9 +3,37 @@ extends CharacterBody2D
 class_name Wizard
 
 @export var health = 1;
-
+@onready var sprite = $Basic_Alchemist_Sprite
+@onready var collision = $CollisionShape2D
+@onready var PointLight = $PointLight2D
+@onready var timer = $Timer
 
 func incoming_damage(damage) -> void:
 	health -= damage
 	if health == 0:
 		queue_free()
+		
+func _process(_delta):
+		if PointLight.player:
+			PointLight.look_at(PointLight.player.global_position)
+			timer.paused = true
+			
+
+func turn_around():
+	if sprite.flip_h:
+		sprite.flip_h = false
+		collision.position.x *= -1
+		PointLight.position.x *= -1
+		PointLight.position.y += 3
+		PointLight.rotation_degrees = -180
+	else:
+		sprite.flip_h = true
+		collision.position.x *= -1
+		PointLight.position.x *= -1
+		PointLight.position.y -= 3
+		PointLight.rotation_degrees = 0
+
+
+func _on_timer_timeout():
+	turn_around()
+	
